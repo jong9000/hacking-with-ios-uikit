88256d40232dfdc88ba98ed45288c98bf35deb6e
Jon Gentry <jonmarkgentry@gmail.com>
2020-12-16 16:57:24 -0800

    project 5 challenges completed

--- 05-Word-Scramble/Project5/ViewController.swift
+++ 05-Word-Scramble/Project5/ViewController.swift
@@ -18,6 +18,8 @@ class ViewController: UITableViewController {
     
     navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
     
+    navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(restartGame))
+    
     if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
       if let startWords = try? String(contentsOf: startWordsURL) {
         allWords = startWords.components(separatedBy: "\n")
@@ -48,6 +50,10 @@ class ViewController: UITableViewController {
     return cell
   }
   
+  @objc func restartGame() {
+    startGame()
+  }
+  
   @objc func promptForAnswer() {
     let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
     ac.addTextField()
@@ -63,37 +69,38 @@ class ViewController: UITableViewController {
   }
   
   func submit(_ answer: String) {
-    let lowerAnswer = answer.lowercased()
     
-    let errorTitle: String
-    let errorMessage: String
+    let lowerAnswer = answer.lowercased()
     
     if isPossible(word: lowerAnswer) {
       if isOriginal(word: lowerAnswer) {
         if isReal(word: lowerAnswer) {
-          usedWords.insert(answer, at: 0)
+          
+          if lowerAnswer.count < 3 {
+            showErrorMessage(title: "Invalid", message: "Word must be at least 3 letters.")
+            return
+          } else if lowerAnswer == title {
+            showErrorMessage(title: "Invalid", message: "Cannot be the same as base word.")
+            return
+          }
+          
+          usedWords.insert(lowerAnswer, at: 0)
           
           let indexPath = IndexPath(row: 0, section: 0)
           tableView.insertRows(at: [indexPath], with: .automatic)
           
           return
         } else {
-          errorTitle = "Word not recognized"
-          errorMessage = "You can't just make them up you know!"
+          showErrorMessage(title: "Word not recognized.", message: "You can't just make them up you know!")
         }
       } else {
-        errorTitle = "Word already used."
-        errorMessage = "Be more original."
+        showErrorMessage(title: "Word already used.", message: "Be more original")
       }
     } else {
       guard let title = title else { return }
-      errorTitle = "Word not possible."
-      errorMessage = "You can't spell that word from \(title.lowercased())."
-    }
-    
-    let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
-    ac.addAction(UIAlertAction(title: "OK", style: .default))
-    present(ac, animated: true)
+      showErrorMessage(title: "Word not possible.",
+                       message: "You can't spell that word from \(title.lowercased()).")
+      }
   }
   
   func isPossible(word: String) -> Bool {
@@ -119,6 +126,12 @@ class ViewController: UITableViewController {
     let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
     
     return misspelledRange.location == NSNotFound
+  }
+  
+  func showErrorMessage(title errorTitle: String, message errorMessage: String) {
+    let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+    ac.addAction(UIAlertAction(title: "OK", style: .default))
+    present(ac, animated: true)
   }
 
 }
