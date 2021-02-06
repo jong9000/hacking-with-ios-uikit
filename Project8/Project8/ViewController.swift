37676555f41edfbc68fd4129c1751cab4cd7609c
Jon Gentry <jonmarkgentry@gmail.com>
2021-02-05 22:25:21 -0800

    project8

---
+++ Project8/Project8/ViewController.swift
@@ -0,0 +1,139 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+  var cluesLabel: UILabel!
+  var answersLabel: UILabel!
+  var currentAnswer: UITextField!
+  var scoreLabel: UILabel!
+  var letterButtons = [UIButton]()
+  
+  var activatedButtons = [UIButton]()
+  var solutions = [String]()
+  
+  var score = 0
+  var level = 1
+  
+  override func loadView() {
+    view = UIView()
+    view.backgroundColor = .white
+    
+    scoreLabel = UILabel()
+    
+    scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+    scoreLabel.textAlignment = .right
+    scoreLabel.text = "Score: 0"
+    view.addSubview(scoreLabel)
+    
+    cluesLabel = UILabel()
+    cluesLabel.translatesAutoresizingMaskIntoConstraints = false
+    cluesLabel.font = UIFont.systemFont(ofSize: 24)
+    cluesLabel.text = "CLUES"
+    cluesLabel.numberOfLines = 0
+    cluesLabel.setContentHuggingPriority(UILayoutPriority(1), for: .vertical)
+    view.addSubview(cluesLabel)
+    
+    currentAnswer = UITextField()
+    currentAnswer.translatesAutoresizingMaskIntoConstraints = false
+    currentAnswer.placeholder = "Tap letters to guess"
+    currentAnswer.textAlignment = .center
+    currentAnswer.font = UIFont.systemFont(ofSize: 44)
+    currentAnswer.isUserInteractionEnabled = false
+    view.addSubview(currentAnswer)
+    
+    answersLabel = UILabel()
+    answersLabel.translatesAutoresizingMaskIntoConstraints = false
+    answersLabel.font = UIFont.systemFont(ofSize: 24)
+    answersLabel.text = "ANSWERS"
+    answersLabel.textAlignment = .right
+    answersLabel.numberOfLines = 0
+    answersLabel.setContentHuggingPriority(UILayoutPriority(1), for: .vertical)
+    view.addSubview(answersLabel)
+    
+    let submit = UIButton(type: .system)
+    submit.translatesAutoresizingMaskIntoConstraints = false
+    submit.setTitle("SUBMIT", for: .normal)
+    submit.addTarget(self, action: #selector(submitTapped), for: .touchUpInside)
+    view.addSubview(submit)
+    
+    let clear = UIButton(type: .system)
+    clear.translatesAutoresizingMaskIntoConstraints = false
+    clear.setTitle("CLEAR", for: .normal)
+    clear.addTarget(self, action: #selector(clearTapped), for: .touchUpInside)
+    view.addSubview(clear)
+    
+    let buttonsView = UIView()
+    buttonsView.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(buttonsView)
+    
+    NSLayoutConstraint.activate([
+      scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+      scoreLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+      
+      cluesLabel.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor),
+      cluesLabel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor, constant: 100),
+      cluesLabel.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.6, constant: -100),
+      
+      answersLabel.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor),
+      answersLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: -100),
+      answersLabel.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.4, constant: -100),
+      answersLabel.heightAnchor.constraint(equalTo: cluesLabel.heightAnchor),
+      
+      currentAnswer.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      currentAnswer.widthAnchor.constraint(equalTo: view.widthAnchor, constant: 0.5),
+      currentAnswer.topAnchor.constraint(equalTo: cluesLabel.bottomAnchor),
+      
+      submit.topAnchor.constraint(equalTo: currentAnswer.bottomAnchor),
+      submit.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -100),
+      submit.heightAnchor.constraint(equalToConstant: 44),
+      
+      clear.centerXAnchor.constraint(equalTo: view.centerXAnchor,constant: 100),
+      clear.centerYAnchor.constraint(equalTo: submit.centerYAnchor),
+      clear.heightAnchor.constraint(equalToConstant: 44),
+      
+      buttonsView.widthAnchor.constraint(equalToConstant: 750),
+      buttonsView.heightAnchor.constraint(equalToConstant: 320),
+      buttonsView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      buttonsView.topAnchor.constraint(equalTo: submit.bottomAnchor, constant: 20),
+      buttonsView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: -20)
+    ])
+    
+    let width = 150
+    let height = 80
+    
+    for row in 0..<5 {
+      for column in 0..<5 {
+        let letterButton = UIButton(type: .system)
+        letterButton.titleLabel?.font = UIFont.systemFont(ofSize: 36)
+        letterButton.setTitle("WWW", for: .normal)
+        letterButton.addTarget(self, action: #selector(letterTapped), for: .touchUpInside)
+        
+        let frame = CGRect(x: column * width, y: row * height, width: width, height: height)
+        letterButton.frame = frame
+        
+        buttonsView.addSubview(letterButton)
+        letterButtons.append(letterButton)
+      }
+    }
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+  @objc func letterTapped(_ sender: UIButton) {
+    
+  }
+  
+  @objc func submitTapped(_ sender: UIButton) {
+    
+  }
+  
+  @objc func clearTapped(_ sender: UIButton) {
+    
+  }
+  
+
+}
+
