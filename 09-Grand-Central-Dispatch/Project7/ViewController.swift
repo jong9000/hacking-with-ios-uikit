220e7b6a7adb2f73aaa5d45911a4495218ffa24b
Jon Gentry <jonmarkgentry@gmail.com>
2021-02-15 16:06:54 -0800

    creeate project9 folder and clone project7 into it

---
+++ 09-Grand-Central-Dispatch/Project7/ViewController.swift
@@ -0,0 +1,82 @@
+import UIKit
+
+class ViewController: UITableViewController {
+
+  var petitions = [Petition]()
+  var filteredPetitions = [Petition]()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(leftBarButtonTapped))
+    navigationItem.setRightBarButton(UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(rightBarButtonTapped)), animated: true)
+    
+    let urlString: String
+    
+    if navigationController?.tabBarItem.tag == 0 {
+      urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+    } else {
+      urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+    }
+    
+    if let url = URL(string: urlString) {
+      if let data = try? Data(contentsOf: url) {
+        parse(json: data)
+        return 
+      }
+    }
+      showError()
+  }
+  
+  @objc func leftBarButtonTapped() {
+    let ac = UIAlertController(title: "Credits", message: "Made with Aloha by jong9000", preferredStyle: .alert)
+    ac.addAction(UIAlertAction(title: "OK", style: .default))
+    present(ac, animated: true)
+  }
+  
+  @objc func rightBarButtonTapped() {
+    let ac = UIAlertController(title: "Search", message: nil, preferredStyle: .alert)
+    ac.addTextField { textField in
+      textField.placeholder = "Search"
+    }
+    
+    ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+    ac.addAction(UIAlertAction(title: "Go", style: .default))
+    
+    present(ac,animated: true)
+  }
+
+  func showError() {
+    let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+    ac.addAction(UIAlertAction(title: "OK", style: .default))
+    present(ac, animated: true)
+  }
+  
+  func parse(json: Data) {
+    let decoder = JSONDecoder()
+    
+    if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+      petitions = jsonPetitions.results
+      tableView.reloadData()
+    }
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return petitions.count
+  }
+
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    let petition = petitions[indexPath.row]
+    cell.textLabel?.text = petition.title
+    cell.detailTextLabel?.text = petition.body
+    return cell
+  }
+  
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    let vc = DetailViewController()
+    vc.detailItem = petitions[indexPath.row]
+    navigationController?.pushViewController(vc, animated: true)
+  }
+
+}
